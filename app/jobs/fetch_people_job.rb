9bb8af1605ecc53569dd4eb430d0309a85dbb285
= <terrymorrow@gmail.com>
2018-10-15 17:56:32 -0400

    Update FetchPeople Job to fetch from SalesLoft API, Create CreatePeople Job to create people after fetching, add HTTParty gem for fetching from API

--- app/jobs/fetch_people_job.rb
+++ app/jobs/fetch_people_job.rb
@@ -2,6 +2,13 @@ class FetchPeopleJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    response = HTTParty.get(
+      "http://api.salesloft.com/v2/people.json",
+      headers: {
+        "Authorization" => "Bearer #{ENV['SALESLOFT_API_KEY']}"
+      }
+    )
+    return JSON.parse(response.body)['data']
   end
+
 end
