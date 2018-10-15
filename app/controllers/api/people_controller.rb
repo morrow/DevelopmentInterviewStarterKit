f9d077e019937d8c2b4802c81b348943fb98ae0b
= <terrymorrow@gmail.com>
2018-10-15 19:02:11 -0400

    Add check for empty data before serving people from /api/people API endpoint, Add check for SALES_LOFT_API_KEY ENV variable to application controller

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -3,6 +3,10 @@ class Api::PeopleController < Api::BaseController
   respond_to :json
 
   def index
+    # check for empty data and run CreatePersonJob if necessary
+    if Person.all.length < 1
+      CreatePeopleJob.perform_now
+    end
     respond_with Person.all.to_json
   end
 
