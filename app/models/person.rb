f3e1cfb17d7f779462c70eb968fa800f408e2a67
= <terrymorrow@gmail.com>
2018-10-15 16:35:15 -0400

    Update tests for Person, add attribute requirements for name, title, unique email

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+
+  validates_presence_of :name, :title
+  validates_presence_of :email, uniqueness: true
+
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  it 'is valid'
-  it 'has a name attribute'
-  it 'has an email attribute'
-  it 'has a job title attribute'
+
+  subject { described_class.new(name: 'Test Name', email: 'test@salesloft.com',title: 'Test Title') }
+
+  it 'is valid with required attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'requires a name attribute' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'requires an email attribute' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'requires a job title attribute' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
 end
