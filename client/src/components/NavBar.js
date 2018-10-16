32194f6219f367836231ed6e72705c974ac1bbaf
= <terrymorrow@gmail.com>
2018-10-15 22:11:23 -0400

    Move CSS into separate css files, using className based styling instead of inline. Reasoning: separation of concerns, DRY when styles are used in more than one component.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,69 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-const NAV_HEIGHT = 46;
-const AVATAR_HEIGHT = 40;
-const AVATAR_WIDTH = 40;
-
-const StyledNavBar = styled.nav`
-  display: flex;
-  justify-content: flex-end;
-  align-content: center;
-
-  margin: 0px;
-  padding 0px;
-  height: ${NAV_HEIGHT}px;
-  width: 100%;
-`;
-
-let NavText = styled.p`
-  color: #4a90e2;
-  padding-right: 10px;
-  padding-left: 10px;
-  color: white;
-`;
-
-const Badge = styled.div`
-  margin: 3px;
-  display: flex;
-  min-width: 152px;
-  align-items: center;
-  overflow: hidden;
-  border-radius: 7px;
-  border 2px solid #4a90e2;
-  justify-content: center;
-  font-size: 25px;
-  font-weight: bold;
-  background-color: #4a90e2;
-`;
-
-let SignInBadge = () => (
-  <Badge>
-    <a href='/users/auth/salesloft' style={{textDecoration: 'none'}}><NavText>Login</NavText></a>
-  </Badge>
-);
-
-let UserBadge = ({user}) => (
-  <Badge>
-    <a href='/users/auth/logout' style={{textDecoration: 'none'}}><NavText>{user.name}</NavText></a>
-  </Badge>
-);
-
-let HomeLink = ()=> (
-  <Link to='/'>Home</Link>
-);
-
-let PeopleLink = ()=> (
-  <Link to='/people'>People</Link>
-)
+let user = ()=> ( <a href='/users/auth/logout'>{user.name}</a> )
+let login = ()=> ( <a href='/users/auth/salesloft'>Login</a> )
 
 let NavBar = ({user}) => (
-  <StyledNavBar>
-    <HomeLink />
-    <PeopleLink />
-    {user ? <UserBadge user={user} /> : <SignInBadge />}
-  </StyledNavBar>
+  <nav>
+    <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+    <NavLink exact to='/people' activeClassName='active'>People</NavLink>
+    {user ? user() : login() }
+  </nav>
 );
 
 export default NavBar
--- client/src/components/PeoplePage.js
+++ client/src/components/PeoplePage.js
@@ -4,24 +4,13 @@ import styled from 'styled-components'
 import { ConnectedNavBar } from '../containers/ConnectedNavBar'
 import { PeopleContainer } from '../containers/PeopleContainer'
 
-const Page = styled.div`
-  display: grid;
-  grid-template 46px 1fr / 1fr;
-  height: 100%;
-  width: 100%;
-`;
-
-const FlexBox = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
 export const PeoplePage = () => (
-  <Page>
+  <div>
     <ConnectedNavBar />
-    <FlexBox>
+    <main>
+      <h1>People</h1>
       <PeopleContainer />
-    </FlexBox>
-  </Page>
+    </main>
+  </div>
 );
