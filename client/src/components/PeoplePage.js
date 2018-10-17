6274c006674418d48ba8ff06e5c8e9d333241e5c
= <terrymorrow@gmail.com>
2018-10-17 09:07:13 -0400

    Add visibility logic to conditionally show/hide possible duplicates on PeoplePage

--- client/src/components/PeoplePage.js
+++ client/src/components/PeoplePage.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
-import { toggleEmailCharacterFrequency } from '../actions/Actions'
+import { toggleEmailCharacterFrequency, togglePossibleDuplicates } from '../actions/Actions'
 import EmailCharacterFrequencyContainer from '../containers/EmailCharacterFrequencyContainer'
+import PossibleDuplicatesContainer from '../containers/PossibleDuplicatesContainer'
 import { ConnectedNavBar } from '../containers/ConnectedNavBar'
 import { PeopleContainer } from '../containers/PeopleContainer'
 import { ActionButtonContainer } from '../containers/ActionButtonContainer'
 
 
 export const PeoplePage = ({app}) => {
-  let button_text = app.email_character_frequency_visible ? 'Hide' : 'Show'
+  const getShowHideText = (bool)=> bool ? 'Hide' : 'Show'
   return(
     <div>
       <ConnectedNavBar />
       <main>
         <h1>People</h1>
         <div className='email-character-frequency-wrap'>
-          <ActionButtonContainer action={toggleEmailCharacterFrequency}>{button_text} e-mail character frequency</ActionButtonContainer>
+          <ActionButtonContainer action={toggleEmailCharacterFrequency}>
+            {getShowHideText(app.email_character_frequency_visible)} e-mail character frequency
+          </ActionButtonContainer>
           { app.email_character_frequency_visible &&
             <EmailCharacterFrequencyContainer />
+          }
+        </div>
+        <div className='possible-duplicates-wrap'>
+          <ActionButtonContainer action={togglePossibleDuplicates}>
+            {getShowHideText(app.possible_duplicates_visible)} possible duplicates
+          </ActionButtonContainer>
+          { app.possible_duplicates_visible &&
+            <PossibleDuplicatesContainer />
           }
         </div>
         <PeopleContainer />
