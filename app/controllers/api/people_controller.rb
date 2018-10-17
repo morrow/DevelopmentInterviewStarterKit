88b25110ec80d5d770174a177dec70dff1dbdd7a
= <terrymorrow@gmail.com>
2018-10-17 11:47:46 -0400

    Set API_KEY in omniauth callback and use API_KEY env variable everywhere instead now.

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -3,13 +3,8 @@ class Api::PeopleController < Api::BaseController
   respond_to :json
 
   def index
-    if user_signed_in? and current_user.token
-      CreatePeopleJob.perform_now(current_user.token.access_token)
-    elsif ENV['SALESLOFT_API_KEY']
-      CreatePeopleJob.perform_now(ENV['SALESLOFT_API_KEY'])
-    else
-      return render json: {}, status: 401
-    end
+    return render json: {}, status: 401 unless ENV['SALESLOFT_API_KEY']
+    CreatePeopleJob.perform_now(ENV['SALESLOFT_API_KEY'])
     respond_with Person.all.sort_by(&:name).to_json
   end
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def salesloft
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    ENV['SALESLOFT_API_KEY'] = @user.token.access_token
     sign_in_and_redirect @user, event: :authentication
   end
 
   def failure
-    errors = { errors: "sign in failed" } 
+    errors = { errors: "sign in failed" }
     render json: errors
   end
 end
