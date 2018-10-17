c97e2b2ccf9443c3f6bb31d8c83a362c57583a31
= <terrymorrow@gmail.com>
2018-10-17 12:20:59 -0400

    Update People controller to conditionally run CreatePeopleJob now or later depending on if there are any Person records in the database or not.

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -4,7 +4,12 @@ class Api::PeopleController < Api::BaseController
 
   def index
     return render json: {}, status: 401 unless ENV['SALESLOFT_API_KEY']
-    CreatePeopleJob.perform_now(ENV['SALESLOFT_API_KEY'])
+     # Check for existence of people, CreatePeople from API now or later
+    if Person.all.length < 1
+      CreatePeopleJob.perform_now(ENV['SALESLOFT_API_KEY'])
+    else
+      CreatePeopleJob.perform_later(ENV['SALESLOFT_API_KEY'])
+    end
     respond_with Person.all.sort_by(&:name).to_json
   end
 
